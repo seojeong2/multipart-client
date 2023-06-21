1591466ec5d807ab27a09e63065acf56f72b695c
seojeong <seojeong1101@gmail.com>
2023-06-21 18:32:27 +0900

    파일 업로드 & 서버 전송 테스트코드

--- multipart-client-test/src/views/FileUpload.vue
+++ multipart-client-test/src/views/FileUpload.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="information">
     <p>파일 업로드</p>
+    <input type="file" @change="selectFile" ref="imgFile" />
+    <button @click="submit">서버에 전송하기</button>
   </div>
 </template>
 
@@ -10,7 +12,6 @@ export default {
   data() {
     return {
       files: [],
-      backendUrl: "http://localhost:8080",
       image: "test_image",
     };
   },
@@ -19,23 +20,38 @@ export default {
       const formData = new FormData();
       formData.append("image", this.image);
 
-      try {
-        const { data } = await axios.post(
-          "http://localhost:8085/upload",
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        console.log(data);
-      } catch (err) {
-        console.log(err);
-      }
+      // try {
+      //   const { data } = await axios.post(
+      //     "http://localhost:8085/upload",
+      //     formData,
+      //     {
+      //       headers: {
+      //         "Content-Type": "multipart/form-data",
+      //       },
+      //     }
+      //   );
+      //   console.log(data);
+      // } catch (err) {
+      //   console.log(err);
+      // }
+      axios
+        .post("/upload", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data;`,
+          },
+          baseURL: "http://localhost:8085",
+        })
+        .then((response) => {
+          console.log("success");
+
+          alert("등록되었습니다!");
+        })
+        .catch((error) => {
+          console.log("error");
+        });
     },
-    selectFile(file) {
-      this.image = file;
+    selectFile(event) {
+      this.image = this.$refs.imgFile.files[0];
     },
   },
 };
